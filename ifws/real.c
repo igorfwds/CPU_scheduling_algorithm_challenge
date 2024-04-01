289bfd8a12a9bde173fe822997e898451c94bee9
Igor <igorfwds@hotmail.com>
2024-04-01 19:56:34 -0300

    descobri o problema com a string, falta tratar

--- ifws/real.c
+++ ifws/real.c
@@ -69,7 +69,6 @@ int main(int argc, char *argv[]) {
 
         fscanf(ptr_file, "%s %d %d", processes[i].name, &processes[i].period, &processes[i].CPU_burst);
 
-        processes[i].remaining_burst = processes[i].CPU_burst;
         processes[i].lost = 0;
         processes[i].completed = 0;
         processes[i].killed = 0;
@@ -81,7 +80,7 @@ int main(int argc, char *argv[]) {
 
     
     if(strcmp(argv[0], "./rate") == 0){
-        printf("\nEXECUTION BY RATE\n");
+        printf("EXECUTION BY RATE\n");
         rate(total_time, processes, p_lines);
 
     }else if(strcmp(argv[0], "./edf") == 0){
@@ -112,32 +111,41 @@ int main(int argc, char *argv[]) {
 void rate(int total_time, Process *processes, int p_lines){
     int time = 0;
     qsort(processes, p_lines, sizeof(Process), rateSort);
-    
-    int i = 0;
+
+    int process = 0;
     while(time <= total_time){
-        
-        if(time > processes[i].deadline){
-            processes[i].deadline += processes[i].period; 
+        if(processes[process].remaining_burst == 0){
+            processes[process].remaining_burst = processes[process].CPU_burst;
         }
-        while(processes[i].remaining_burst > 0){
-            if(time >= processes[i].deadline){
-                processes[i].lost++;
-                processes[i].deadline += processes[i].period;
+        
+        if(time > processes[process].deadline){
+            processes[process].deadline += processes[process].period; 
+        }
+
+        while(processes[process].remaining_burst > 0){
+            if(time >= processes[process].deadline){
+                processes[process].lost++;
+                processes[process].deadline += processes[process].period;
+                break;
             }
+    
             time++;
-            processes[i].remaining_burst--;
-            if(processes[i].remaining_burst == 0){
-                processes[i].completed++;
-                processes[i].feedback[0] = 'F';
-                i++;
+            processes[process].remaining_burst--;
+            if(processes[process].remaining_burst == 0){
+                processes[process].completed++;
+                processes[process].feedback[0] = 'F';
+                break;
             }
         }
         
+
+        process++; //logica certa mas lixo de cpu entrando na string do nome do primeiro processo
         time++;
     }
 
+
     for(int p = 0; p < p_lines; p++){
-        printf("[%s] for %d units - %s\n", processes[p].name, processes[p].lost, processes[p].feedback);
+        printf("[%s] for %d units - %s\n", processes[p].name, (processes[p].CPU_burst - processes[p].remaining_burst), processes[p].feedback);
     }
 
     
